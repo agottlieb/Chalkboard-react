2c42bb7725ca7344f228c16792f1cc6c86b1867c
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-20 14:45:45 -0400

    adding callback function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import "./styles.css";
 
 class Chalkboard extends Component {
   state = {
-    chalk: ""
+    chalk: "",
+    notes: []
   };
+
+  render() {
+    var notes = this.state.notes.map((note) => <li> {note} </li>);
+    //a different typle callback- defines the function in the variable itself
+    //does not go back to a group of notes stored in an array
+    //creates a one-line for each note string being stored
+  }
 }
